a6baf014ec315189cade35a0cf2f00097e7cce89
Andries16 <84147378+Andries16@users.noreply.github.com>
2021-05-17 20:55:56 +0300

    Update app/View/Components/blockProMenu.php
    Co-authored-by: Dumitru Arsenii <arsdu24@gmail.com>

--- app/View/Components/blockProMenu.php
+++ app/View/Components/blockProMenu.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class blockProMenu extends Component
+class BlockProMenu extends Component
 {
     /**
      * Create a new component instance.
