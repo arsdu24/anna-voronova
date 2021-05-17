3748bec8699237647c0180d3e108b746544256e0
Andries16 <84147378+Andries16@users.noreply.github.com>
2021-05-17 22:18:05 +0300

    Update app/View/Components/headingGroup.php
    Co-authored-by: Dumitru Arsenii <arsdu24@gmail.com>

--- app/View/Components/headingGroup.php
+++ app/View/Components/headingGroup.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class headingGroup extends Component
+class HeadingGroup extends Component
 {
     /**
      * Create a new component instance.
