cd263c93846ca03e490898bbed8383cb4bba568c
Andries16 <84147378+Andries16@users.noreply.github.com>
2021-05-17 22:17:56 +0300

    Update app/View/Components/collItem.php
    Co-authored-by: Dumitru Arsenii <arsdu24@gmail.com>

--- app/View/Components/collItem.php
+++ app/View/Components/collItem.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class collItem extends Component
+class CollItem extends Component
 {
     /**
      * Create a new component instance.
