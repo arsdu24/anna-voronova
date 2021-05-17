176aaea1a48ed3c45fe3f5b8d655d50d433e2663
Andries16 <84147378+Andries16@users.noreply.github.com>
2021-05-17 22:18:20 +0300

    Update app/View/Components/proContent.php
    Co-authored-by: Dumitru Arsenii <arsdu24@gmail.com>

--- app/View/Components/proContent.php
+++ app/View/Components/proContent.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class proContent extends Component
+class ProContent extends Component
 {
     /**
      * Create a new component instance.
