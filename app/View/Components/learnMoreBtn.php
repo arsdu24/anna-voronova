9eb3f9e6d68007827f89b21951da723b533e319d
Andries16 <84147378+Andries16@users.noreply.github.com>
2021-05-17 22:18:10 +0300

    Update app/View/Components/learnMoreBtn.php
    Co-authored-by: Dumitru Arsenii <arsdu24@gmail.com>

--- app/View/Components/learnMoreBtn.php
+++ app/View/Components/learnMoreBtn.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class learnMoreBtn extends Component
+class LearnMoreBtn extends Component
 {
     /**
      * Create a new component instance.
