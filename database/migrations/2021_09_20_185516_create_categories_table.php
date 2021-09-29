05d3e6fca0db0ff89ca6eeffc9f9516ba1129221
Andrei Sîrbu <aaw2012sirbu.andrei@gmail.com>
2021-09-29 20:31:34 +0300

    AVS-48 (#46)

--- database/migrations/2021_09_20_185516_create_categories_table.php
+++ database/migrations/2021_09_20_185516_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCollectionsTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,22 +13,17 @@ class CreateCollectionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('collections', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('thumbnail');
-            $table->string('title');
-            $table->string('description');
+            $table->string('name');
+            $table->boolean('in_menu')->nullable();
+            $table->string('excerpt')->nullable();
             $table->timestamps();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+    
     public function down()
     {
-        Schema::dropIfExists('collections');
+        Schema::dropIfExists('categories');
     }
 }
