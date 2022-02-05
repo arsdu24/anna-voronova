46de266a4dc2c2a352719a0bc7f54bcae1d495e3
Andrei Sîrbu <aaw2012sirbu.andrei@gmail.com>
2022-02-05 18:00:10 +0200

    AVS-70

--- app/Http/Controllers/ContactUsController.php
+++ app/Http/Controllers/ContactUsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Category;
 use App\Collection;
+use App\ShippingDetail;
 use App\Product;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -27,7 +28,33 @@ class ContactUsController extends Controller
         $menu_categories = Category::where('in_menu',1)->orderby('id','desc')->take(5)->get();
         $menu_collections = Collection::where('in_menu',1)->orderby('id','desc')->take(2)->get();
         $site = SiteSettings::first();
-        return view('pages.contact-us',['categories'=>$categories,'user'=>$user,'cart'=>$cart,'menu_products'=>$menu_products,'menu_categories'=>$menu_categories,'menu_collections'=>$menu_collections,'site'=>$site,'collections'=>$collections]);
+        $details = ShippingDetail::all();
+        if($details->count()==0){
+            DB::table('shipping_details')->insert([
+                [
+                    'icon'=>'fas fa-plane-departure',
+                    'title'=>'Free Worldwide Shipping',
+                    'subtitle'=>'On all orders over $75.00',
+                    'description'=>'Free Worldwide Shipping'
+                ],
+                [
+                    'icon'=>'far fa-credit-card',
+                    'title'=>'100% Payment Secure',
+                    'subtitle'=>'We ensure secure payment with PEV',
+                    'description'=>'100% Payment Secure'
+
+                ],
+                [
+                    'icon'=>'fas fa-share-square',
+                    'title'=>'30 Days Return',
+                    'subtitle'=>'Return it within 20 day for an exchange',
+                    'description'=>'30 Days Return'
+
+                ]
+            ]);
+            $details = ShippingDetail::all();
+        }
+        return view('pages.contact-us',['details'=>$details,'categories'=>$categories,'user'=>$user,'cart'=>$cart,'menu_products'=>$menu_products,'menu_categories'=>$menu_categories,'menu_collections'=>$menu_collections,'site'=>$site,'collections'=>$collections]);
     }
     public function sendEmail(Request $request)
     {
